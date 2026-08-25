ee71d5631a2ed0afe881019e1cdc9c056d661205
Gabriel Bispo <gabrieldasilvabispo@hotmail.com>
2026-08-25 16:54:36 -0300

    Add files via upload

---
+++ UCs/Java Script/olamundo.js
@@ -0,0 +1,42 @@
+console.log("Olá mundo");
+var a=10, b=5; // Criando variaveis
+console.log(a); // comando usado para exibir o valor que esta na variavel a
+console.log(b); // comando usado para exibir o valor que esta na variavel b
+console.log("O resultado da soma de a + b é: ",a+b); // comando usado para exibir o resultado do calculo da adição
+console.log(a-b); // comando usado para exibir o resultado do calculo da subtração
+console.log("O resultado da subtração de a - b é: ",a-b);
+console.log(a*b); // comando usado para exibir o resultado do calculo da multiplicação
+console.log("O resultado da multiplicação de a * b é: ",a*b);
+console.log(a/b); // comando usado para exibir o resultado do calculo da divisão
+console.log("O resultado da divisão a / b é: ",a/b);
+console.log(a%b); // comando usado para exibir o resultado do calculo 
+console.log("O resultado do resto da divisão a % b é: ",a%b);
+console.log(a**b); // comando usado para exibir o resultado do calculo
+console.log("O resultado da soma de a ** b é: ",a**b);
+
+// *** Regras de Sintaxe ***  
+// Outra forma de declarar variaveis
+// Podemos usar o comando var ou let
+let x = 5;
+let y = 6;
+
+let z = x + y;
+console.log(z);
+
+// Os numeros podem ser exibidos da seguinte forma:
+// 10 - número inteiro
+// 10.50 - número decimal
+
+//  As Strings são texto, e podem ser escritas entre aspas duplas ou simples
+// "Gabriel"
+// 'Gabriel'
+
+// Variáveis
+let numero;
+numero = 10;
+
+// O JavaScript é sensivel a Maiúsculas e Minúsculas
+// Exemplo:
+let nome = "Gabriel";
+let Nome = "Bispo";
+
