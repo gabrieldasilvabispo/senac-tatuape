29df03971ddcc80450b06d0e3c655f0e915ca843
Gabriel Bispo <gabrieldasilvabispo@hotmail.com>
2026-09-23 15:27:08 -0300

    Add files via upload

---
+++ UCs/Java/hello.java
@@ -0,0 +1,9 @@
+/** Para fazer comentario 
+ * Hello World
+ * Gabriel Da Silva Bispo*/
+
+public class hello{
+    public static void main (String[] args ){
+        System.out.println("hello World");
+    }  
+}
