f11af3a5cc201591af434b1ed32aee985bf5f002
Gabriel Bispo <gabrieldasilvabispo@hotmail.com>
2026-09-23 15:27:34 -0300

    Delete UCs/Java/hello.java

--- UCs/Java/hello.java
+++
@@ -1,9 +0,0 @@
-/** Para fazer comentario 
- * Hello World
- * Gabriel Da Silva Bispo*/
-
-public class hello{
-    public static void main (String[] args ){
-        System.out.println("hello World");
-    }  
-}
